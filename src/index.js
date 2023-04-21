d34e89a78727942f173ff9892449ccdef590700c
Emma Gerigscott <elgerig@gmail.com>
2023-04-21 10:33:04 -0700

    add form for user input and run getCurrency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import CurrencyService from "./js/currency-service";
 import { CountryCodes, CountryInfo } from "./js/countrycodes";
 
+// Business Logic
+
 function getCurrency(baseCode, targetCode, amount) {
   CurrencyService.getExchangeRate(baseCode, targetCode, amount)
     .then(response => {
@@ -17,13 +19,14 @@ function getCurrency(baseCode, targetCode, amount) {
 
 function getCountryCodes() {
   CurrencyService.getCountryCodes()
-  .then(response => {
-    if (response.supported_codes) {
-      const supportedCodes = addCountryCodes(response.supported_codes);
-    } else {
-      console.log(response);
-    }
-  });
+    .then(response => {
+      if (response.supported_codes) {
+        const supportedCodes = addCountryCodes(response.supported_codes);
+        console.log(supportedCodes)
+      } else {
+        console.log(response);
+      }
+    });
 }
 
 function addCountryCodes(result) {
@@ -34,9 +37,19 @@ function addCountryCodes(result) {
     const countryInfo = new CountryInfo(code, country);
     countryCodes.addCode(countryInfo);
   });
-  return countryCodes
+  return countryCodes;
 }
 
+function handleSubmitEvent(e) {
+  e.preventDefault();
+  const baseCode = document.querySelector("#base-code").value;
+  const targetCode = document.querySelector("#target-code").value;
+  const amount = parseFloat(document.querySelector("#amount").value);
 
-getCurrency('EUR', 'GBP', 16);
+  getCurrency(baseCode, targetCode, amount);
+}
+
 getCountryCodes();
+window.addEventListener("load", function() {
+  this.document.querySelector("form").addEventListener("submit", handleSubmitEvent);
+})
