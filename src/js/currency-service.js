55382736cd79619fe996e2dd5ee75b25c5a13a3c
Emma Gerigscott <elgerig@gmail.com>
2023-04-21 09:31:48 -0700

    add method to fetch country codes from API

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -6,14 +6,27 @@ export default class CurrencyService {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCode}/${targetCode}/${amount}`)
       .then(response => {
         if (!response.ok) {
-          const errorMessage = `${response.status} ${response.statusText}`
-          throw new Error(errorMessage)
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
         }
-        return response.json()
+        return response.json();
       })
       .catch(error => {
-        console.log(error)
-        return error
+        return error;
       });
+  }
+
+  static async getCountryCodes() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`)
+      .then(response => {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        return error;
+      })
   }
 }
