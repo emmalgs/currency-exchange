d5c4c09624e811a74343c0a232b839b8bc2bce07
Emma Gerigscott <elgerig@gmail.com>
2023-04-21 10:12:13 -0700

    test five pass adds multiple instances of countries and codes to countryCodes object

--- __tests__/countrycodes.test.js
+++ __tests__/countrycodes.test.js
@@ -31,6 +31,7 @@ describe('CountryCodes', () => {
     country = 'United States';
     countryInfo = new CountryInfo(code, country);
     countryCodes = new CountryCodes();
+    countryCodes.addCode(countryInfo);
   });
 
   test('it should create an instance of CountryCodes object when called', () => {
@@ -38,9 +39,24 @@ describe('CountryCodes', () => {
   });
 
   test('it should add a country and code when method addCode is called', () => {
-    countryCodes.addCode(countryInfo);
+    
     expect(countryCodes.codes).toEqual({ USD: 'United States'});
   });
 
-  
+  test('it should add multiple countries to countryCodes', () => {
+    let code2 = 'AED';
+    let country2 = 'UAE Dirham';
+    let countryInfo2 = new CountryInfo(code2, country2);
+    let code3 = 'AFN';
+    let country3 = 'Afghan Afghani';
+    let countryInfo3 = new CountryInfo(code3, country3);
+    let code4 = 'ALL';
+    let country4 = 'Albanian Lek';
+    let countryInfo4 = new CountryInfo(code4, country4);
+    countryCodes.addCode(countryInfo2);
+    countryCodes.addCode(countryInfo3);
+    countryCodes.addCode(countryInfo4);
+    console.log(countryCodes.codes);
+    expect(countryCodes.codes).toEqual( { USD: 'United States', AED: 'UAE Dirham', AFN: 'Afghan Afghani', ALL: 'Albanian Lek' })
+  })  
 })
