fba2a50a6b084efcb563c562d6d33ea9e3f2bfc5
Emma Gerigscott <elgerig@gmail.com>
2023-04-21 09:01:45 -0700

    fetch first api call to get exchange rate, remove currencies file as there is an api call to get all source codes

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,1 +1,15 @@
-// https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCode}/${targetCode}/${amount} this takes starting currency, what you want to exchange to, and the amount
+// https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCode}/${targetCode}/${amount} this takes starting currency, what you want to exchange to, and the amount. base_code, target_code, and conversion_rate are keys in the response object.
+// https://v6.exchangerate-api.com/v6/YOUR-API-KEY/codes returns a json file of all supported currency codes and country name, outputs an array of an array at supported_codes
+
+export default class CurrencyService {
+  static async getExchangeRate(baseCode, targetCode, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCode}/${targetCode}/${amount}`)
+      .then(response => {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`
+          throw new Error(errorMessage)
+        }
+        return response.json()
+      });
+  }
+}
