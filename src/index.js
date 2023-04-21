85bc3b10426ca481f24ecd529a82d8b50aa47859
Emma Gerigscott <elgerig@gmail.com>
2023-04-21 10:18:54 -0700

    loop through all supported country codes from api fetch for user logic use and input validation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import CurrencyService from "./js/currency-service";
+import { CountryCodes, CountryInfo } from "./js/countrycodes";
 
 function getCurrency(baseCode, targetCode, amount) {
   CurrencyService.getExchangeRate(baseCode, targetCode, amount)
@@ -18,12 +19,24 @@ function getCountryCodes() {
   CurrencyService.getCountryCodes()
   .then(response => {
     if (response.supported_codes) {
-      console.log(response.supported_codes);
+      const supportedCodes = addCountryCodes(response.supported_codes);
     } else {
       console.log(response);
     }
-  })
+  });
 }
+
+function addCountryCodes(result) {
+  const countryCodes = new CountryCodes();
+  result.forEach(element => {
+    let code = element[0];
+    let country = element[1];
+    const countryInfo = new CountryInfo(code, country);
+    countryCodes.addCode(countryInfo);
+  });
+  return countryCodes
+}
+
 
 getCurrency('EUR', 'GBP', 16);
 getCountryCodes();
