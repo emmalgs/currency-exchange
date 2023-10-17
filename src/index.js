6c262fddb2497010655855a28e1fffaec8302046
Emma Gerigscott <elgerig@gmail.com>
2023-10-17 09:52:41 -0400

    reformat

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './css/styles.css';
+import "./css/styles.css";
 import CurrencyService from "./js/currency-service";
 import { CountryCodes, CountryInfo } from "./js/countrycodes";
 
@@ -18,21 +18,21 @@ function displayBigNums(number) {
 
 function getCurrency(baseCode, targetCode, amount) {
   CurrencyService.getExchangeRate(baseCode, targetCode, amount, apiKey)
-    .then(response => {
+    .then((response) => {
       if (response instanceof Error) {
         const errorMessage = `Something is not right`;
         throw new Error(errorMessage);
       }
       printResults(response, amount);
     })
-    .catch(error => {
+    .catch((error) => {
       printError(error);
     });
 }
 
 function getCountryCodes() {
   CurrencyService.getCountryCodes(apiKey)
-    .then(response => {
+    .then((response) => {
       if (response instanceof Error) {
         const errorMessage = `Something is not right`;
         throw new Error(errorMessage);
@@ -42,14 +42,14 @@ function getCountryCodes() {
         printCountryCodeOptions(codes);
       }
     })
-    .catch(error => {
+    .catch((error) => {
       printError(error);
     });
 }
 
 function validateCode(code) {
   CurrencyService.getCountryCodes()
-    .then(response => {
+    .then((response) => {
       if (response instanceof Error) {
         const errorMessage = `Oh no! Somethings wrong`;
         throw new Error(errorMessage);
@@ -57,9 +57,9 @@ function validateCode(code) {
       const codesList = addCountryCodes(response.supported_codes);
       return codesList;
     })
-    .then(response2 => {
+    .then((response2) => {
       const codes = Object.keys(response2.codes);
-      code.forEach(element => {
+      code.forEach((element) => {
         if (!codes.includes(element)) {
           const errorMessage = `Sorry, ${element} is not a valid code`;
           throw new Error(errorMessage);
@@ -67,14 +67,14 @@ function validateCode(code) {
       });
       printCurrencyNames([response2.codes[code[0]], response2.codes[code[1]]]);
     })
-    .catch(error => {
+    .catch((error) => {
       printError(error);
     });
 }
 
 function addCountryCodes(result) {
   const countryCodes = new CountryCodes();
-  result.forEach(element => {
+  result.forEach((element) => {
     let code = element[0];
     let country = element[1];
     const countryInfo = new CountryInfo(code, country);
@@ -86,7 +86,7 @@ function addCountryCodes(result) {
 function printCountryCodeOptions(ctryCodes) {
   let codes = Object.keys(ctryCodes.codes);
   let codesArray = Array.from(codes);
-  codesArray.forEach(element => {
+  codesArray.forEach((element) => {
     printCodeInputs(element, ctryCodes);
   });
 }
@@ -109,19 +109,27 @@ function checkNumberInputs(input) {
 
 function printCurrencyNames(code) {
   const outputDiv = document.querySelector("#output");
-  outputDiv.querySelector(".base-code").innerText = `Conversion for ${code[0].toUpperCase()} to ${code[1].toUpperCase()}`;
+  outputDiv.querySelector(
+    ".base-code"
+  ).innerText = `Conversion for ${code[0].toUpperCase()} to ${code[1].toUpperCase()}`;
 }
 
 function printResults(response, amount) {
   const outputDiv = document.querySelector("#output");
-  outputDiv.querySelector(".amount-entered").innerText = `${response.base_code}: $${displayBigNums(financial(amount))}`;
-  outputDiv.querySelector(".exg-rate").innerText = `Conversion Rate: ${response.conversion_rate}`;
-  outputDiv.querySelector(".exg-result").innerText = `${response.target_code}: $${displayBigNums(financial(response.conversion_result))}`;
+  outputDiv.querySelector(".amount-entered").innerText = `${
+    response.base_code
+  }: $${displayBigNums(financial(amount))}`;
+  outputDiv.querySelector(
+    ".exg-rate"
+  ).innerText = `Conversion Rate: ${response.conversion_rate}`;
+  outputDiv.querySelector(".exg-result").innerText = `${
+    response.target_code
+  }: $${displayBigNums(financial(response.conversion_result))}`;
 }
 
 function printCodeInputs(code, countryCodes) {
   const datalist = document.querySelectorAll("datalist");
-  datalist.forEach(list => {
+  datalist.forEach((list) => {
     let option = document.createElement("option");
     option.value = code;
     option.innerText = countryCodes.codes[code];
@@ -148,8 +156,12 @@ function resetError() {
 function handleSubmitEvent(e) {
   e.preventDefault();
   resetError();
-  const baseCode = checkTextInputs(document.querySelector("input[list='base-code']").value);
-  const targetCode = checkTextInputs(document.querySelector("input[list='target-code']").value);
+  const baseCode = checkTextInputs(
+    document.querySelector("input[list='base-code']").value
+  );
+  const targetCode = checkTextInputs(
+    document.querySelector("input[list='target-code']").value
+  );
   let amount = document.querySelector("#amount").value;
   amount = checkNumberInputs(amount);
   if (!baseCode || !targetCode || !amount) {
@@ -163,6 +175,8 @@ function handleSubmitEvent(e) {
 }
 
 window.addEventListener("load", function () {
-  this.document.querySelector("form").addEventListener("submit", handleSubmitEvent);
+  this.document
+    .querySelector("form")
+    .addEventListener("submit", handleSubmitEvent);
   getCountryCodes();
 });
